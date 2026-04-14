0b6d7d6504bd14d9cda183831c161c2340b320e2
TkYYYY1514 <TkYYYY1514@users.noreply.github.com>
2026-04-14 15:16:02 +0000

    deploy: c0dc21f54b7d349c5194b5aed98be141d5e7606b

---
+++ assets/console-D1HSCcS8.js
@@ -0,0 +1,1 @@
+import{L as e,d as t,m as n,t as r}from"./_plugin-vue_export-helper-SWhK-GNp.js";var i={};function a(r,i){return e(),n(`div`,null,[...i[0]||=[t(`h2`,null,`工作台`,-1)]])}var o=r(i,[[`render`,a]]);export{o as default};
