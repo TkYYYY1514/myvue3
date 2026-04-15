00e8991fa101125bbb0d8b2c10bbfbccbaada44d
Your Name <you@example.com>
2026-04-15 15:27:09 +0800

    修改可使用线上mock

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue' 
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(),
     viteMockServe({
       mockPath: 'mock',
-      enable: true, // 👈 关键！让 mock 打包后也能运行
-    }),
+      enable: true,
+      logger: false
+    })
   ],
   base: '/myvue3/',
   resolve: {
